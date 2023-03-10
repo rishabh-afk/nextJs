14870049814affcf223e65ccfacebfefe8371fc3
Rishabh gupta <rishabh@innovatia.co.in>
2023-03-10 10:18:18 +0530

    add pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Layout from "@/components/Layout";
 import Head from "next/head";
 
 export default function Home() {
@@ -9,7 +10,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello world</h1>
+      <Layout>
+        <h1>E-Commerce Website</h1>
+      </Layout>
     </>
   );
 }
---
+++ pages/product/[slug].tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const Slug = () => {
+  const router = useRouter();
+  const { slug } = router.query;
+  return <div>Slug</div>;
+};
+
+export default Slug;
